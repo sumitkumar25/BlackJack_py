3641601c82c3f1eb387768d98e58739d472e14b6
sumit <sumitchauhancoe@gmail.com>
2018-10-05 22:41:07 +0530

    scaled down blackjack

--- blackjack.py
+++ blackjack.py
@@ -9,6 +9,7 @@ class BlackJack:
         self.d = Player('Dealer', 2)
         self.deck = Deck()
         self.dealInitialHand()
+        self.gameEnd = False
         self.displayCards()
         self.dealHand()
 
@@ -16,33 +17,63 @@ class BlackJack:
         self.p.cards.append(self.deck.getCard(False))
         self.d.cards.append(self.deck.getCard(False))
         self.p.cards.append(self.deck.getCard(False))
-        self.d.cards.append(self.deck.getCard(True))
+        self.d.cards.append(self.deck.getCard(False))
+        self.p.updateState()
+        self.d.updateState()
 
     def displayCards(self):
         for gamePlayer in [self.p, self.d]:
-            print(gamePlayer.name + '  has: ')
+            print(gamePlayer.name + '  has: total ' + str(gamePlayer.count))
             for c in gamePlayer.cards:
                 if(not c.hidden):
                     print('    '+str(c.value) + ' of ' + c.suite)
                 else:
                     print("    hidden Card")
 
-    def dealHand(self):
-        self.p.updateState()
+    def dealPlayer(self):
+        playerAction = input(self.p.name + ' Hit or Miss ')
+        if(playerAction.lower() == 'hit'):
+            self.p.cards.append(self.deck.getCard(False))
+            self.p.updateState()
+        elif(playerAction.lower() == 'miss'):
+            pass
+        else:
+            print('invalid input please retry')
+            self.dealPlayer()
+        self.displayCards()
+
+
+    def dealDealer(self):
+        self.d.cards.append(self.deck.getCard(False))
         self.d.updateState()
-        while(self.p.count < 21 and self.d.count < 21):
-            if(len(self.p.cards) >= len(self.d.cards)):
-                playerAction = input(self.p.name + 'Hit or Miss')
-                if(playerAction.lower() == 'hit'):
-                    self.p.cards.append(self.deck.getCard(True))
-                elif(playerAction.lower() == 'miss'):
-                    pass
-                else:
-                    continue
-            else:
-                self.p.cards.append(self.deck.getCard(True))
+        print('dealer dealt')
+        self.displayCards()
+
+    def checkGame(self):
+        pc = self.p.count
+        pd = self.d.count
+        winner = None
+        loser = None
+        if(pc > 21 and pd <= 21):
+            self.gameEnd = True
+            loser = self.p
+            winner = self.d
+        elif(pd > 21 and pc <= 21):
+            self.gameEnd = True
+            loser = self.d
+            winner = self.p
+
+        if(self.gameEnd):
+            print('winner is {0} with score {1} and loser is {2} with score {3}'.format(
+                winner.name, winner.count, loser.name, loser.count))
+
+    def dealHand(self):
+        while(not self.gameEnd):
+            self.dealPlayer()
+            self.checkGame()
+            self.dealDealer()
+            self.checkGame()
 
 
-
-game=BlackJack()
+game = BlackJack()
 game.initGame()
--- player.py
+++ player.py
@@ -6,4 +6,14 @@ class Player:
         self.count = 0
 
     def updateState(self):
-        self.count = len(self.cards)
+        self.count = 0
+        for card in self.cards:
+            try:
+                cardVal = int(card.value)
+            except ValueError:
+                if(card == 'ace' and self.count > 11):
+                    cardVal = 1
+                else:
+                    print('cardval11')
+                    cardVal = 11
+            self.count += cardVal
